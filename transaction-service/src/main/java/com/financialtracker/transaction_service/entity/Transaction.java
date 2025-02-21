f051ec223d4e36bb1e3a0e6cf2ed0cb6d6131fbf
Bogdan <wwwterleckiy@gmail.com>
2025-02-21 23:21:22 +0200

    add transaction entity

--- transaction-service/src/main/java/com/financialtracker/transaction_service/entity/Transaction.java
+++ transaction-service/src/main/java/com/financialtracker/transaction_service/entity/Transaction.java
@@ -1,4 +1,112 @@
 package com.financialtracker.transaction_service.entity;
 
+import com.financialtracker.transaction_service.enums.TransactionType;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@SequenceGenerator(name = "transaction_seq", sequenceName = "transaction_id_seq", allocationSize = 1)
+@Table(name = "transactions")
+@Entity
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "transaction_seq")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType type;
+
+    @Column(nullable = false, precision = 15, scale = 2)
+    private BigDecimal amount;
+
+    private String description;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    public Transaction() {}
+
+    public Transaction(Long id,
+                       TransactionType type,
+                       BigDecimal amount,
+                       String description,
+                       Long userId,
+                       LocalDateTime createdAt,
+                       LocalDateTime updatedAt) {
+        this.id = id;
+        this.type = type;
+        this.amount = amount;
+        this.description = description;
+        this.userId = userId;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
