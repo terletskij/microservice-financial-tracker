cee752a80744d956ae008b69cedab00a1096a258
Bogdan <wwwterleckiy@gmail.com>
2025-02-22 01:18:39 +0200

    add custom exception ResourceNotFound

---
+++ transaction-service/src/main/java/com/financialtracker/transaction_service/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.financialtracker.transaction_service.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
