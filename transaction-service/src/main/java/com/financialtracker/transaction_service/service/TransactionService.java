5780273f2e80feeba6fc5d3e402c0147c39f28c5
Bogdan <wwwterleckiy@gmail.com>
2025-02-23 01:06:57 +0200

    add TransactionService implementation

--- transaction-service/src/main/java/com/financialtracker/transaction_service/service/TransactionService.java
+++ transaction-service/src/main/java/com/financialtracker/transaction_service/service/TransactionService.java
@@ -1,4 +1,19 @@
 package com.financialtracker.transaction_service.service;
 
-public class TransactionService {
+import com.financialtracker.transaction_service.dto.request.CreateTransactionRequest;
+import com.financialtracker.transaction_service.dto.request.UpdateTransactionRequest;
+import com.financialtracker.transaction_service.entity.Transaction;
+
+import java.util.List;
+
+public interface TransactionService {
+    Transaction getTransactionById(Long id);
+
+    List<Transaction> getAllTransactionsByUserId(Long userId);
+
+    Transaction createTransaction(CreateTransactionRequest request, Long userId);
+
+    void deleteTransactionById(Long id);
+
+    Transaction updateTransactionById(Long id, UpdateTransactionRequest request);
 }
--- transaction-service/src/main/java/com/financialtracker/transaction_service/service/TransactionServiceImpl.java
+++ transaction-service/src/main/java/com/financialtracker/transaction_service/service/TransactionServiceImpl.java
@@ -1,5 +1,7 @@
 package com.financialtracker.transaction_service.service;
 
+import com.financialtracker.transaction_service.dto.request.CreateTransactionRequest;
+import com.financialtracker.transaction_service.dto.request.UpdateTransactionRequest;
 import com.financialtracker.transaction_service.entity.Transaction;
 import com.financialtracker.transaction_service.exceptions.ResourceNotFoundException;
 import com.financialtracker.transaction_service.repository.TransactionRepository;
@@ -10,17 +12,42 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class TransactionService {
+public class TransactionServiceImpl implements TransactionService{
 
     private final TransactionRepository transactionRepository;
 
+    @Override
     public Transaction getTransactionById(Long id) {
         return transactionRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Transaction not found"));
     }
 
-    public List<Transaction> getAllTransactionsByUserId() {
-        return transactionRepository.findAll();
+    @Override
+    public List<Transaction> getAllTransactionsByUserId(Long userId) {
+        return transactionRepository.findAllByUserId(userId);
     }
 
-    public Transaction 
+    @Override
+    public Transaction createTransaction(CreateTransactionRequest request, Long userId) {
+        Transaction transaction = new Transaction();
+        transaction.setType(request.getType());
+        transaction.setAmount(request.getAmount());
+        transaction.setDescription(request.getDescription());
+        transaction.setUserId(userId);
+
+        return transactionRepository.save(transaction);
+    }
+
+    @Override
+    public void deleteTransactionById(Long id) {
+        transactionRepository.deleteById(id);
+    }
+
+    @Override
+    public Transaction updateTransactionById(Long id, UpdateTransactionRequest request) {
+        Transaction transaction = getTransactionById(id);
+        transaction.setType(request.getType());
+        transaction.setAmount(request.getAmount());
+        transaction.setDescription(request.getDescription());
+        return transactionRepository.save(transaction);
+    }
 }
