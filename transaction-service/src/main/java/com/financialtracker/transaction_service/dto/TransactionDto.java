bd42803a328bbffead9cf51c5c24fc765b32b12f
Bogdan <wwwterleckiy@gmail.com>
2025-02-23 00:41:27 +0200

    add TransactionDto

--- transaction-service/src/main/java/com/financialtracker/transaction_service/dto/TransactionDto.java
+++ transaction-service/src/main/java/com/financialtracker/transaction_service/dto/TransactionDto.java
@@ -1,4 +1,23 @@
 package com.financialtracker.transaction_service.dto;
 
+import com.financialtracker.transaction_service.enums.TransactionType;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
 public class TransactionDto {
+
+    private TransactionType type;
+
+    private BigDecimal amount;
+
+    private String description;
+
+    private Long userId;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 }
