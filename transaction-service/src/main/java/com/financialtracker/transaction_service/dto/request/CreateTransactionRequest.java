5b58da865eb75809ff8d91a40b38ebc83e4628a1
Bogdan <wwwterleckiy@gmail.com>
2025-02-23 00:44:20 +0200

    feat(transaction): add CreateTransactionRequest and UpdateTransactionRequest

--- transaction-service/src/main/java/com/financialtracker/transaction_service/dto/request/CreateTransactionRequest.java
+++ transaction-service/src/main/java/com/financialtracker/transaction_service/dto/request/CreateTransactionRequest.java
@@ -1,4 +1,22 @@
 package com.financialtracker.transaction_service.dto.request;
 
+import com.financialtracker.transaction_service.enums.TransactionType;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class CreateTransactionRequest {
+    @NotNull(message = "Transaction type is required")
+    private TransactionType type;
+
+    @NotNull(message = "Amount is required")
+    @Min(value = 0, message = "Amount must be positive")
+    private BigDecimal amount;
+
+    @NotBlank(message = "Description is required")
+    private String description;
 }
--- transaction-service/src/main/java/com/financialtracker/transaction_service/dto/request/UpdateTransactionRequest.java
+++ transaction-service/src/main/java/com/financialtracker/transaction_service/dto/request/UpdateTransactionRequest.java
@@ -1,4 +1,20 @@
 package com.financialtracker.transaction_service.dto.request;
 
+import com.financialtracker.transaction_service.enums.TransactionType;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class UpdateTransactionRequest {
+
+    private TransactionType type;
+
+    @Min(value = 0, message = "Amount must be positive")
+    private BigDecimal amount;
+
+    private String description;
 }
