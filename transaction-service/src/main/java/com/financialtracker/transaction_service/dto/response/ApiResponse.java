286b6a6de767b2be97b83c89eafbc9f18d17032f
Bogdan <wwwterleckiy@gmail.com>
2025-02-24 21:27:31 +0200

    Add ApiResponse to standardize API responses with message and data fields

---
+++ transaction-service/src/main/java/com/financialtracker/transaction_service/dto/response/ApiResponse.java
@@ -0,0 +1,16 @@
+package com.financialtracker.transaction_service.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApiResponse {
+    private String message;
+    private Object data;
+
+    public ApiResponse(String message, Object data) {
+        this.message = message;
+        this.data = data;
+    }
+}
