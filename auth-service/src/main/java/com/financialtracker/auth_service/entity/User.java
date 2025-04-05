01ecb96122aa9e95c9027455f87f13b8e7e6238b
Bogdan <wwwterleckiy@gmail.com>
2025-04-05 23:30:38 +0300

    add user entity

--- auth-service/src/main/java/com/financialtracker/auth_service/entity/User.java
+++ auth-service/src/main/java/com/financialtracker/auth_service/entity/User.java
@@ -1,4 +1,28 @@
 package com.financialtracker.auth_service.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@SequenceGenerator(name = "users_seq", sequenceName = "users_id_seq", allocationSize = 1)
+@Table(name = "users")
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_seq")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
 }
