34ea6cf47a974bf78bb3b94648c4f05cc986895f
Bogdan <wwwterleckiy@gmail.com>
2025-04-05 12:37:02 +0300

    auth-service init

---
+++ auth-service/src/test/java/com/financialtracker/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financialtracker.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
