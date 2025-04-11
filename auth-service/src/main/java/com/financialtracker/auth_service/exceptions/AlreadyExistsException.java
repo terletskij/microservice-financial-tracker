4ed2230523c86552b2af049d00006f40435e983a
Bogdan <wwwterleckiy@gmail.com>
2025-04-12 00:24:20 +0300

    add AlreadyExistsException

--- auth-service/src/main/java/com/financialtracker/auth_service/exceptions/AlreadyExistsException.java
+++ auth-service/src/main/java/com/financialtracker/auth_service/exceptions/AlreadyExistsException.java
@@ -1,7 +1,7 @@
 package com.financialtracker.auth_service.exceptions;
 
 public class AlreadyExistsException extends RuntimeException {
-  public AlreadyExistsException(String message) {
-    super(message);
-  }
+    public AlreadyExistsException(String message) {
+        super(message);
+    }
 }
