73a1dde82363d8c1c8d62386e7ac6ebac848bf8b
Bogdan <wwwterleckiy@gmail.com>
2025-04-13 00:28:40 +0300

    implement JwtService

--- auth-service/src/main/java/com/financialtracker/auth_service/security/jwt/JwtService.java
+++ auth-service/src/main/java/com/financialtracker/auth_service/security/jwt/JwtService.java
@@ -1,4 +1,85 @@
 package com.financialtracker.auth_service.security.jwt;
 
+import com.financialtracker.auth_service.dto.JwtAuthenticationDto;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
 public class JwtService {
+
+    @Value("${JWT_SECRET}")
+    private String jwtSecret;
+
+    @Value("${JWT_EXPIRATION_TIME_IN_MILLIS}")
+    private int expirationTime;
+
+    @Value("${JWT_REFRESH_EXPIRATION_TIME_IN_MILLIS}")
+    private long refreshTokenExpiration;
+
+    public JwtAuthenticationDto generateAuthToken(String email) {
+        JwtAuthenticationDto jwtDto = new JwtAuthenticationDto();
+        jwtDto.setToken(generateJwtToken(email));
+        jwtDto.setRefreshToken(generateRefreshToken(email));
+        return jwtDto;
+    }
+
+    public JwtAuthenticationDto refreshBaseToken(String email, String refreshToken) {
+        JwtAuthenticationDto jwtDto = new JwtAuthenticationDto();
+        jwtDto.setToken(generateJwtToken(email));
+        jwtDto.setRefreshToken(refreshToken);
+        return jwtDto;
+    }
+
+    public boolean validateJwtToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(getSignKey())
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+            return true;
+        } catch (JwtException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getEmailFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .verifyWith(getSignKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return claims.getSubject();
+    }
+
+    private String generateJwtToken(String email) {
+        Date expirationDate = new Date(System.currentTimeMillis() + expirationTime);
+        return Jwts.builder()
+                .subject(email)
+                .expiration(expirationDate)
+                .signWith(getSignKey())
+                .compact();
+    }
+
+    private String generateRefreshToken(String email) {
+        Date expirationDate = new Date(System.currentTimeMillis() + expirationTime);
+        return Jwts.builder()
+                .subject(email)
+                .expiration(expirationDate)
+                .signWith(getSignKey())
+                .compact();
+    }
+
+    private SecretKey getSignKey() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
 }
