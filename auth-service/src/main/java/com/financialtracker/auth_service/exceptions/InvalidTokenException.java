d195148bcd4fd432afb64df75ade0477b2f4e70f
Bogdan <wwwterleckiy@gmail.com>
2025-04-22 01:58:42 +0300

    add custom exceptions

--- auth-service/src/main/java/com/financialtracker/auth_service/exceptions/InvalidTokenException.java
+++ auth-service/src/main/java/com/financialtracker/auth_service/exceptions/InvalidTokenException.java
@@ -1,7 +1,7 @@
 package com.financialtracker.auth_service.exceptions;
 
 public class InvalidTokenException extends RuntimeException {
-  public InvalidTokenException(String message) {
-    super(message);
-  }
+    public InvalidTokenException(String message) {
+        super(message);
+    }
 }
