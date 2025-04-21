52a1ad13020b37aa8eed9c232dfc83198f4eab88
Bogdan <wwwterleckiy@gmail.com>
2025-04-22 02:00:46 +0300

    add UserDto

--- auth-service/src/main/java/com/financialtracker/auth_service/dto/UserDto.java
+++ auth-service/src/main/java/com/financialtracker/auth_service/dto/UserDto.java
@@ -1,4 +1,26 @@
 package com.financialtracker.auth_service.dto;
 
+import com.financialtracker.auth_service.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class UserDto {
+    private Long id;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 30, message = "Username must be between 3 and 30 characters")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    private Role role;
+    
+    public UserDto() {}
 }
