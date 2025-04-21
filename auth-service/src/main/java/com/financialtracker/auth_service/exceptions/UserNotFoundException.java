a0d8a19d1fea936e95d55413bca572b79dcb4c41
Bogdan <wwwterleckiy@gmail.com>
2025-04-22 01:58:17 +0300

    add ApiResponse to standardize response

---
+++ auth-service/src/main/java/com/financialtracker/auth_service/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.financialtracker.auth_service.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+  public UserNotFoundException(String message) {
+    super(message);
+  }
+}
