af2592097de2a279838d684b0ebb8b434307039e
Bogdan <wwwterleckiy@gmail.com>
2025-04-22 01:21:21 +0300

    implemented getAuthorities

--- auth-service/src/main/java/com/financialtracker/auth_service/security/CustomUserDetails.java
+++ auth-service/src/main/java/com/financialtracker/auth_service/security/CustomUserDetails.java
@@ -2,16 +2,18 @@ package com.financialtracker.auth_service.security;
 
 import com.financialtracker.auth_service.entity.User;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public record CustomUserDetails(User user) implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return Collections.singletonList(new SimpleGrantedAuthority(user.getRole().name()));
     }
 
     @Override
