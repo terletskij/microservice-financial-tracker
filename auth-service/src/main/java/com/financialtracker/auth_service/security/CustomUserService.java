67957bb2b83cda633da065ac043f6dcfd22716c6
Bogdan <wwwterleckiy@gmail.com>
2025-04-22 00:07:19 +0300

    add CustomUserService with loadUserByUsername

--- auth-service/src/main/java/com/financialtracker/auth_service/security/CustomUserService.java
+++ auth-service/src/main/java/com/financialtracker/auth_service/security/CustomUserService.java
@@ -1,4 +1,21 @@
 package com.financialtracker.auth_service.security;
 
-public class CustomUserService {
+import com.financialtracker.auth_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public CustomUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
 }
