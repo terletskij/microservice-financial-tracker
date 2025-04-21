a6fe6b6b5f04fc114d1e37bada09f146e66c46f4
Bogdan <wwwterleckiy@gmail.com>
2025-04-22 00:06:39 +0300

    add CustomUserDetails implements UserDetails with basic functionality

---
+++ auth-service/src/main/java/com/financialtracker/auth_service/security/CustomUserDetails.java
@@ -0,0 +1,26 @@
+package com.financialtracker.auth_service.security;
+
+import com.financialtracker.auth_service.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public record CustomUserDetails(User user) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
---
+++ auth-service/src/main/java/com/financialtracker/auth_service/security/CustomUserService.java
@@ -0,0 +1,4 @@
+package com.financialtracker.auth_service.security;
+
+public class CustomUserService {
+}
