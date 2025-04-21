cb8c24a8fe8979912e9279955f9225bb9f0ce6b2
Bogdan <wwwterleckiy@gmail.com>
2025-04-22 00:40:04 +0300

    add new functionality

--- auth-service/src/main/java/com/financialtracker/auth_service/service/UserService.java
+++ auth-service/src/main/java/com/financialtracker/auth_service/service/UserService.java
@@ -1,11 +1,19 @@
 package com.financialtracker.auth_service.service;
 
+import com.financialtracker.auth_service.dto.JwtAuthenticationDto;
+import com.financialtracker.auth_service.dto.RefreshTokenDto;
+import com.financialtracker.auth_service.dto.request.LoginRequest;
 import com.financialtracker.auth_service.dto.request.RegisterRequest;
 import com.financialtracker.auth_service.entity.User;
+import org.apache.tomcat.websocket.AuthenticationException;
 
 import java.util.List;
 
 public interface UserService {
+
+    JwtAuthenticationDto logIn(LoginRequest loginRequest) throws AuthenticationException;
+
+    JwtAuthenticationDto refreshToken(RefreshTokenDto refreshTokenDto) throws AuthenticationException;
 
     User createUser(RegisterRequest request);
 
--- auth-service/src/main/java/com/financialtracker/auth_service/service/impl/UserServiceImpl.java
+++ auth-service/src/main/java/com/financialtracker/auth_service/service/impl/UserServiceImpl.java
@@ -1,24 +1,48 @@
 package com.financialtracker.auth_service.service.impl;
 
+import com.financialtracker.auth_service.dto.JwtAuthenticationDto;
+import com.financialtracker.auth_service.dto.RefreshTokenDto;
+import com.financialtracker.auth_service.dto.request.LoginRequest;
 import com.financialtracker.auth_service.dto.request.RegisterRequest;
 import com.financialtracker.auth_service.entity.User;
 import com.financialtracker.auth_service.enums.Role;
 import com.financialtracker.auth_service.exceptions.AlreadyExistsException;
 import com.financialtracker.auth_service.repository.UserRepository;
+import com.financialtracker.auth_service.security.jwt.JwtService;
 import com.financialtracker.auth_service.service.UserService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.websocket.AuthenticationException;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    private final JwtService jwtService;
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
     private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public JwtAuthenticationDto logIn(LoginRequest loginRequest) throws AuthenticationException {
+        User user = findByCredentials(loginRequest);
+        return jwtService.generateAuthToken(user.getEmail());
+    }
+
+    @Override
+    public JwtAuthenticationDto refreshToken(RefreshTokenDto refreshTokenDto) throws AuthenticationException {
+        String refreshToken = refreshTokenDto.getRefreshToken();
+        if (refreshToken != null && jwtService.validateJwt(refreshToken)) {
+            User user = findByEmail(jwtService.getEmailFromToken(refreshToken));
+            return jwtService.refreshBaseToken(user.getEmail(), refreshToken);
+        }
+        throw new AuthenticationException("Invalid refresh token");
+    }
 
     @Override
     public User createUser(RegisterRequest request) {
@@ -26,7 +50,7 @@ public class UserServiceImpl implements UserService {
             throw new AlreadyExistsException("User with this email already exists");
         }
         User user = modelMapper.map(request, User.class);
-        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRole(Role.USER);
         return userRepository.save(user);
     }
@@ -36,4 +60,18 @@ public class UserServiceImpl implements UserService {
         return userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("User not found"));
     }
 
+    private User findByCredentials(LoginRequest request) throws AuthenticationException {
+        Optional<User> optionalUser = userRepository.findByEmail(request.getEmail());
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            if (passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+                return user;
+            }
+        }
+        throw new AuthenticationException("Email or password are incorrect");
+    }
+
+    private User findByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(() -> new EntityNotFoundException("User not found"));
+    }
 }
