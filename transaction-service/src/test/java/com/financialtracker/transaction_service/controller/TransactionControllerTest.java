79dadb9dfc8b14f1614360843a59e3fe18e2136f
Bogdan <wwwterleckiy@gmail.com>
2025-04-26 02:05:57 +0300

    test: update integration tests for TransactionController with JWT

--- transaction-service/src/test/java/com/financialtracker/transaction_service/controller/TransactionControllerTest.java
+++ transaction-service/src/test/java/com/financialtracker/transaction_service/controller/TransactionControllerTest.java
@@ -1,30 +1,24 @@
 package com.financialtracker.transaction_service.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.financialtracker.transaction_service.dto.TransactionDto;
 import com.financialtracker.transaction_service.dto.request.CreateTransactionRequest;
 import com.financialtracker.transaction_service.dto.request.UpdateTransactionRequest;
-import com.financialtracker.transaction_service.entity.Transaction;
 import com.financialtracker.transaction_service.enums.TransactionType;
-import com.financialtracker.transaction_service.service.TransactionService;
 import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.math.BigDecimal;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,33 +28,36 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 public class TransactionControllerTest {
 
-    // TODO: Replace X-User-Id header with JWT authentication after implementing Auth Service
-
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
+    @BeforeEach
+    void setUp() {
+        SecurityContextHolder.getContext().setAuthentication(
+                new org.springframework.security.authentication.UsernamePasswordAuthenticationToken(2L, null, null)
+        );
+    }
+
     @Test
     @DisplayName("POST [201 created] /transactions - Create transaction successfully")
     void testCreateTransaction() throws Exception {
-
         CreateTransactionRequest request = new CreateTransactionRequest();
         request.setType(TransactionType.EXPENSE);
         request.setAmount(new BigDecimal("100.23"));
         request.setDescription("Test desc");
 
         mockMvc.perform(post("/transactions")
-                .header("X-User-Id", "2")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.message").value("Transaction successfully created"))
                 .andExpect(jsonPath("$.data.id").exists())
                 .andExpect(jsonPath("$.data.userId").value(2))
                 .andExpect(jsonPath("$.data.type").value("EXPENSE"))
-                .andExpect(jsonPath("$.data.amount").value(new BigDecimal("100.23")));
+                .andExpect(jsonPath("$.data.amount").value(100.23));
     }
 
     @Test
@@ -70,12 +67,9 @@ public class TransactionControllerTest {
         request.setDescription("Missing required fields");
 
         mockMvc.perform(post("/transactions")
-                .header("X-User-Id", "2")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request)))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.message").value("Validation error"))
-                .andExpect(jsonPath("$.data.type").exists());
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
     }
 
     @Test
@@ -87,14 +81,12 @@ public class TransactionControllerTest {
         request.setDescription("Test desc");
 
         MvcResult creationResult = mockMvc.perform(post("/transactions")
-                .header("X-User-Id", "3")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isCreated())
                 .andReturn();
 
         String responseContent = creationResult.getResponse().getContentAsString();
-
         int id = JsonPath.read(responseContent, "$.data.id");
 
         mockMvc.perform(get("/transactions/" + id))
@@ -110,34 +102,51 @@ public class TransactionControllerTest {
                 .andExpect(status().isNotFound())
                 .andExpect(jsonPath("$.message").value("Transaction not found"));
     }
-//    TODO: fix after user security integration
-//    @Test
-//    @DisplayName("GET [401 Unauthorized] /transactions - Returns 401 when no user ID is provided")
-//    void testGetAllTransactions_Unauthorized() throws Exception {
-//        mockMvc.perform(get("/transactions"))
-//                .andExpect(status().isUnauthorized());
-//    }
+
+    @Test
+    @DisplayName("GET [401 Unauthorized] /transactions - Returns 401 when no user ID is provided")
+    void testGetAllTransactions_Unauthorized() throws Exception {
+        SecurityContextHolder.clearContext();
+
+        mockMvc.perform(get("/transactions"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @DisplayName("GET [200 OK] /transactions - Retrieves all transactions successfully")
+    void testGetAllTransactions_Success() throws Exception {
+        CreateTransactionRequest request = new CreateTransactionRequest();
+        request.setType(TransactionType.EXPENSE);
+        request.setAmount(new BigDecimal("100.23"));
+        request.setDescription("Test desc");
+
+        mockMvc.perform(post("/transactions")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(get("/transactions"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("All transactions successfully received"))
+                .andExpect(jsonPath("$.data").isArray());
+    }
 
     @Test
     @DisplayName("PUT [200 OK] /transactions/{id} - Updates a transaction successfully")
     void testUpdateTransaction_Success() throws Exception {
-
         CreateTransactionRequest createRequest = new CreateTransactionRequest();
         createRequest.setType(TransactionType.EXPENSE);
         createRequest.setAmount(new BigDecimal("100.23"));
         createRequest.setDescription("Test desc");
 
         MvcResult creationResult = mockMvc.perform(post("/transactions")
-                        .header("X-User-Id", "3")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(createRequest)))
-                        .andExpect(status().isCreated())
-                        .andReturn();
+                .andExpect(status().isCreated())
+                .andReturn();
 
         String responseContent = creationResult.getResponse().getContentAsString();
-
         int id = JsonPath.read(responseContent, "$.data.id");
-
 
         UpdateTransactionRequest request = new UpdateTransactionRequest();
         request.setType(TransactionType.INCOME);
@@ -145,8 +154,8 @@ public class TransactionControllerTest {
         request.setDescription("Test update");
 
         mockMvc.perform(put("/transactions/" + id)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.message").value("Transaction update success"));
     }
@@ -160,8 +169,8 @@ public class TransactionControllerTest {
         request.setDescription("Test update");
 
         mockMvc.perform(put("/transactions/777")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isNotFound())
                 .andExpect(jsonPath("$.message").value("Transaction not found"));
     }
@@ -175,15 +184,14 @@ public class TransactionControllerTest {
         createRequest.setDescription("Test desc");
 
         MvcResult creationResult = mockMvc.perform(post("/transactions")
-                        .header("X-User-Id", "3")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(createRequest)))
                 .andExpect(status().isCreated())
                 .andReturn();
 
         String responseContent = creationResult.getResponse().getContentAsString();
-
         int id = JsonPath.read(responseContent, "$.data.id");
+
         mockMvc.perform(delete("/transactions/" + id))
                 .andExpect(status().isNoContent());
     }
@@ -195,6 +203,4 @@ public class TransactionControllerTest {
                 .andExpect(status().isNotFound())
                 .andExpect(jsonPath("$.message").value("Transaction not found"));
     }
-
-
 }
