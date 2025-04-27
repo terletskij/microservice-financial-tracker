3b587c7879dab85f516a601e8b1f0dbbfed2887a
Bogdan <wwwterleckiy@gmail.com>
2025-04-28 01:38:41 +0300

    refactor: rename and move JwtAuthenticationDto to dto/response/JwtAuthenticationResponse

--- auth-service/src/main/java/com/financialtracker/auth_service/dto/response/JwtAuthenticationResponse.java
+++ auth-service/src/main/java/com/financialtracker/auth_service/dto/response/JwtAuthenticationResponse.java
@@ -1,9 +1,9 @@
-package com.financialtracker.auth_service.dto;
+package com.financialtracker.auth_service.dto.response;
 
 import lombok.Data;
 
 @Data
-public class JwtAuthenticationDto {
+public class JwtAuthenticationResponse {
     private String token;
     private String refreshToken;
 }
